cff608da4482f56578c866e2e825bbc98059d845
Lauren Suh <lauren.suh@rangle.io>
2017-03-09 19:18:04 -0500

    something done

--- src/Doggo.js
+++ src/Doggo.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import Title from './title';
+import MatchImages from './match-images';
+import Button from './button.js';
+import Person1 from './assets/Lauren_Suh.jpg'
+import Dog1 from './assets/lauren_dog.png'
+
+
 
 class Doggo extends Component {
   render() {
     return (
-      <p>Hello</p>
+      <div>
+        <Title />
+        <MatchImages image={Person1} />
+        <MatchImages image={Dog1} />
+        <Button>YES</Button>
+        <Button>NO</Button>
+      </div>
     )
   }
 }
